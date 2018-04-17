962dce2e3f71936b2bc22e11d1a62fc1e35db193
Tomek Banach <tomek@banciur.org>
2018-04-18 00:04:44 +0200

    Add transfers route

--- token_state_relational_mapper/mapper/__init__.py
+++ token_state_relational_mapper/mapper/__init__.py
@@ -1,4 +1,4 @@
 from .mapper import Mapper
 from .mapper_options import MapperOptions
 from .event_analyzer import TransferEventAnalyzer
-from .queries import get_token, get_top_token_holders
+from .queries import get_token, get_top_token_holders, get_transfers
